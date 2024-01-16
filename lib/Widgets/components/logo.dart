940d3bac0398c6fab293a0fe05c25f4ff7a6fdac
EricNguyen1203 <21120132@student.hcmus.edu.vn>
2024-01-16 19:04:06 +0700

    add nav bar

--- lib/Widgets/components/logo.dart
+++ lib/Widgets/components/logo.dart
@@ -6,8 +6,8 @@ class AppLogoWidget extends StatelessWidget {
     return Center(
       child: Container(
         // Đặt kích thước hoặc cách căn chỉnh theo nhu cầu của bạn
-        width: 300.0,
-        height: 300.0,
+        width: 180.0,
+        height: 180.0,
         // Thay thế AssetImage bằng Image.network hoặc các widget hình ảnh khác tùy thuộc vào nguồn ảnh của bạn
         child: Image.asset('assets/logo.png'),
         // Các thuộc tính khác như padding, margin có thể được thêm vào tùy thuộc vào yêu cầu của bạn
