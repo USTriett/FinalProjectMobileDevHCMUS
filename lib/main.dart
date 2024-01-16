551133457d1df7c1522ff6f377fb28638dfd0512
EricNguyen1203 <21120132@student.hcmus.edu.vn>
2024-01-16 22:17:22 +0700

    Change Theme Nav Bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:next_food/Bloc/States/swiper_states/swiper_states.dart';
 import 'package:next_food/Bloc/swiper_bloc.dart';
 import 'package:next_food/DAO/food_dao.dart';
@@ -10,6 +12,8 @@ import 'package:next_food/Themes/theme_manager.dart';
 import 'package:next_food/Widgets/components/food_card.dart';
 import 'package:next_food/Widgets/components/foods_swiper.dart';
 import 'package:next_food/Widgets/components/logo.dart';
+import 'package:next_food/Widgets/pages/SignInPage.dart';
+import 'package:next_food/nextfood_icons.dart';
 
 
 import 'Widgets/pages/HomePage.dart';
@@ -42,62 +46,46 @@ class _MyAppState extends State<MyApp> {
   int _page = 0;
   GlobalKey<CurvedNavigationBarState> _bottomNavigationKey = GlobalKey();
 
+  final screens = [
+    HomePage(),
+    SignUpPage(),
+    SignInPage(),
+    HomePage(),
+    SignUpPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        bottomNavigationBar: CurvedNavigationBar(
-          key: _bottomNavigationKey,
-          index: 0,
-          height: 60.0,
-          items: <Widget>[
-            Container(
-              height: 50,
-              child: Column(
-                children: [Icon(Icons.add, size: 30), Text("page 1")],
-              ),
-            ),
-            Container(
-              height: 50,
-              child: Column(
-                children: [Icon(Icons.list, size: 30), Text("page 2")],
-              ),
-            ),
-            Icon(Icons.compare_arrows, size: 30),
-            Icon(Icons.call_split, size: 30),
-            Icon(Icons.perm_identity, size: 30),
-          ],
-          color: Colors.white,
-          buttonBackgroundColor: Colors.white,
-          backgroundColor: Colors.blueAccent,
-          animationCurve: Curves.easeInOut,
-          animationDuration: Duration(milliseconds: 600),
-          onTap: (index) {
-            setState(() {
-              _page = index;
-            });
-          },
-          letIndexChange: (index) => true,
-        ),
-        body: Container(
-          color: Colors.blueAccent,
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(_page.toString(), textScaleFactor: 10.0),
-                ElevatedButton(
-                  child: Text('go to page of index 1'),
-                  onPressed: () {
-                    final CurvedNavigationBarState? navbarstate =
-                        _bottomNavigationKey.currentState;
-                    navbarstate?.setPage(1);
-                  },
-                )
-              ],
-            ),
+      home: SafeArea(
+        top: true,
+        child: Scaffold(
+          extendBody: true,
+          bottomNavigationBar: CurvedNavigationBar(
+            backgroundColor: Colors.transparent,
+            key: _bottomNavigationKey,
+            index: 0,
+            height: 60.0,
+            items: <Widget>[
+              Icon(Icons.home, size: 30, color: ThemeConstants.backgroundColor,),
+              Icon(FontAwesomeIcons.faceGrinSquintTears, size: 30, color: ThemeConstants.backgroundColor,),
+              Icon(Nextfood.nextFoodIcon, size: 40, color: Colors.black,),
+              Icon(FontAwesomeIcons.clockRotateLeft, size: 30, color: ThemeConstants.backgroundColor,),
+              Icon(Icons.settings, size: 30, color: ThemeConstants.backgroundColor,),
+            ],
+            color: ThemeConstants.buttonTextColor,
+            buttonBackgroundColor: ThemeConstants.buttonTextColor,
+            animationCurve: Curves.easeInOut,
+            animationDuration: Duration(milliseconds: 600),
+            onTap: (index) {
+              setState(() {
+                _page = index;
+              });
+            },
+            letIndexChange: (index) => true,
           ),
-        ))
+          body: screens[_page]),
+      )
 
     );
 }
